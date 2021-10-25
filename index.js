879386344167f3d840f1796469760ddca1f1b043
mohanreddy1666 <mohannallamilli11@gmail.com>
2021-10-25 18:28:22 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+/* 
+    TV Guide
+    
+    Write an async function called findShow(query)
+        performs a fetch call to:
+        https://api.tvmaze.com/singlesearch/shows?q=${query}&embed=seasons
+        and returns the resulting show object
+        
+    Build a layout to display the show
+        - Title
+        - Summary
+        - Seasons listed as individual divs
+*/
+
+async function findShow(query) {
+    let response = await fetch(`https://api.tvmaze.com/singlesearch/shows?q=${query}&embed=seasons`)
+    let data = await response.json()
+    return data
+}
+
+findShow("office").then(show => {
+    console.log(show)
+    document.body.innerHTML = `<div class="my-show">
+        <div class="my-show-title">
+            ${show.name}
+        </div>
+        
+        <div class="my-show-summary">
+            ${show.summary}
+        </div>
+        
+        ${show._embedded.seasons.map(season => {
+            return `<div class="my-show-season">Season ${season.number}</div>`
+        }).join('')}
+    </div>`
+})
